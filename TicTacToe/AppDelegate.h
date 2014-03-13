780ccbf96f6dfbdacac858104ee02ba530cca29e
Manas Pradhan <manaspradhan@Manass-MacBook-Air.local>
2014-03-13 13:41:20 -0500

    Initial Commit

---
+++ TicTacToe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by Manas Pradhan on 3/13/14.
+//  Copyright (c) 2014 Manas Pradhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TicTacToe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TicTacToe
+//
+//  Created by Manas Pradhan on 3/13/14.
+//  Copyright (c) 2014 Manas Pradhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
