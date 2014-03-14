df6b97c29b5b56b9647be0baf6d2091ad1ea4b01
Manas Pradhan <manaspradhan@Manass-MacBook-Air.local>
2014-03-13 21:51:53 -0500

    sixth commit

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -23,7 +23,7 @@
 }
 @property (weak, nonatomic) NSString *exOrOh;
 @property (assign, nonatomic) NSInteger gameState;
-@property (assign, nonatomic) BOOL newGame;
+@property (assign, nonatomic) BOOL decrementedOnce;
 @property (strong, nonatomic) IBOutlet UIImageView *ticTacToeBoard;
 
 //@property (strong, nonatomic) IBOutlet UILabel *whichPlayerLabel;
@@ -47,10 +47,10 @@
     myLabelSeven.text = nil;
     myLabelEight.text = nil;
     myLabelNine.text = nil;
-    whichPlayerLabel.text = @"Player 1 - BEGIN";
+    whichPlayerLabel.text = @"Player X - BEGIN";
     self.exOrOh = @"X";
     self.gameState = 0;
-    self.newGame = YES;
+    self.decrementedOnce = NO;
 }
 
 - (IBAction)onLabelTapped:(UITapGestureRecognizer *)tapGestureRecognizer
@@ -59,15 +59,18 @@
     CGPoint point = [tapGestureRecognizer locationInView:self.view];
     [self findLabelUsingPoint:point];
     
-    if (self.gameState % 2 == 0)
+    if (self.gameState < 9)
     {
-        self.exOrOh = @"O";
-        whichPlayerLabel.text = @"Player 2's Turn (O)";
-    }
-    else
-    {
-        self.exOrOh = @"X";
-        whichPlayerLabel.text = @"Player 1's Turn (X)";
+        if (self.gameState % 2 == 0)
+        {
+            self.exOrOh = @"O";
+            whichPlayerLabel.text = @"Player O";
+        }
+        else
+        {
+            self.exOrOh = @"X";
+            whichPlayerLabel.text = @"Player X";
+        }
     }
     
     ++self.gameState;
@@ -82,60 +85,116 @@
         {
             if (myLabelOne.text == nil)
                 myLabelOne.text = self.exOrOh;
+            else if (myLabelOne.text != nil && self.decrementedOnce == NO)
+            {
+                --self.gameState;
+                self.decrementedOnce = YES;
+            }
         }
+        else self.decrementedOnce = NO;
         
         if (CGRectContainsPoint(myLabelTwo.frame, point))
         {
             if (myLabelTwo.text == nil)
                 myLabelTwo.text = self.exOrOh;
+            else if (myLabelTwo.text != nil && self.decrementedOnce == NO)
+            {
+                --self.gameState;
+                self.decrementedOnce = YES;
+            }
         }
+        else self.decrementedOnce = NO;
         
         if (CGRectContainsPoint(myLabelThree.frame, point))
         {
             if (myLabelThree.text == nil)
                 myLabelThree.text = self.exOrOh;
+            else if (myLabelThree.text != nil && self.decrementedOnce == NO)
+            {
+                --self.gameState;
+                self.decrementedOnce = YES;
+            }
         }
+        else self.decrementedOnce = NO;
     
         if (CGRectContainsPoint(myLabelFour.frame, point))
         {
             if (myLabelFour.text == nil)
                 myLabelFour.text = self.exOrOh;
+            else if (myLabelFour.text != nil && self.decrementedOnce == NO)
+            {
+                --self.gameState;
+                self.decrementedOnce = YES;
+            }
         }
+        else self.decrementedOnce = NO;
     
         if (CGRectContainsPoint(myLabelFive.frame, point))
         {
             if (myLabelFive.text == nil)
                 myLabelFive.text = self.exOrOh;
+            else if (myLabelFive.text != nil && self.decrementedOnce == NO)
+            {
+                --self.gameState;
+                self.decrementedOnce = YES;
+            }
         }
+        else self.decrementedOnce = NO;
 
         if (CGRectContainsPoint(myLabelSix.frame, point))
         {
             if (myLabelSix.text == nil)
                 myLabelSix.text = self.exOrOh;
+            else if (myLabelSix.text != nil && self.decrementedOnce == NO)
+            {
+                --self.gameState;
+                self.decrementedOnce = YES;
+            }
         }
+        else self.decrementedOnce = NO;
 
         if (CGRectContainsPoint(myLabelSeven.frame, point))
         {
             if (myLabelSeven.text == nil)
                 myLabelSeven.text = self.exOrOh;
+            else if (myLabelSeven.text != nil && self.decrementedOnce == NO)
+            {
+                --self.gameState;
+                self.decrementedOnce = YES;
+            }
         }
+        else self.decrementedOnce = NO;
     
         if (CGRectContainsPoint(myLabelEight.frame, point))
         {
             if (myLabelEight.text == nil)
                 myLabelEight.text = self.exOrOh;
+            else if (myLabelEight.text != nil && self.decrementedOnce == NO)
+            {
+                --self.gameState;
+                self.decrementedOnce = YES;
+            }
         }
+        else self.decrementedOnce = NO;
     
         if (CGRectContainsPoint(myLabelNine.frame, point))
         {
             if (myLabelNine.text == nil)
                 myLabelNine.text = self.exOrOh;
+            else if (myLabelNine.text != nil && self.decrementedOnce == NO)
+            {
+                --self.gameState;
+                self.decrementedOnce = YES;
+            }
         }
+        else self.decrementedOnce = NO;
         
     }
     else
-        NSLog(@"GAME OVER");
-    
+    {
+        whichPlayerLabel.textColor = [UIColor redColor];
+        whichPlayerLabel.text = @"IT'S A TIE";
+    }
 }
 
 - (void)didReceiveMemoryWarning
